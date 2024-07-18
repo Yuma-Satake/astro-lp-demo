abf7889035e2759cc760977ce629f6eeccf52a37
Yuma Satake <satous.y@icloud.com>
2024-07-18 18:40:46 +0900

    feat: astro 導入

--- src/features/index/IndexPage.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-
-/**
- * IndexPage
- */
-export const IndexPage: FC = () => {
-  return <div>IndexPage</div>;
-};
--- src/theme/theme.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme } from '@mui/material';
-
-export const theme = createTheme({
-  typography: {
-    fontFamily: ['"Noto Sans JP"', 'sans-serif'].join(','),
-  },
-});
